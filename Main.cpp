50e0fee3ef92ae1421ba5838979f1b070483067b
chenshtynmetz <93136824+chenshtynmetz@users.noreply.github.com>
2022-03-14 17:16:23 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include "mat.hpp"
+#include <stdexcept>
+
+using namespace std;
+
+int main(){
+    int col = 0;
+    int row = 0;
+    char a; 
+    char b;
+    string carpet = "";
+    string ans = "";
+    cout << "Welcome to our carpets store!\nPlease enter the size of the carpet that you want- the number of columns and rows" << endl;
+    cin >> col;
+    cin >> row; 
+    cout << "Please enter 2 symbols to the pattern of the carpet:" << endl;
+    cin >> a;
+    cin >> b;
+    try{
+        carpet = ariel::mat(col, row, a, b);
+        cout << "This is your carpet:" << endl << carpet << endl;
+    }
+    catch (runtime_error) {
+        cout << "this input is illegal" << endl;
+    }
+    while (1)
+    {
+        cout << "Do you want another carpet? write yes/no" << endl;
+        cin >> ans;
+        if(ans == "no"){
+            break;
+        }
+        cout << "Please enter the size of the carpet that you want- the number of columns and rows" << endl;
+        cin >> col;
+        cin >> row; 
+        cout << "Please enter 2 symbols to the pattern of the carpet:" << endl;
+        cin >> a;
+        cin >> b;
+    try{
+        carpet = ariel::mat(col, row, a, b);
+        cout << "This is your carpet:" << endl << carpet << endl;
+    }
+    catch (runtime_error) {
+        cout << "this input is illegal" << endl;
+    }
+    }
+    cout << "Thank you of visit in our store!\nhave a good day!" << endl;
+    return 0;
+}
